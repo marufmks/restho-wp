692ed48055bfadc81f1480fe3da546ee371c549d
Maruf Khan <marufmks5556@gmail.com>
2023-01-21 17:06:07 +0600

    added id change of enque file

--- includes/functions/enqueue.php
+++ includes/functions/enqueue.php
@@ -16,71 +16,71 @@ if ( ! function_exists( 'egns_scripts' ) ) {
 		
 		//style
 
-		wp_enqueue_style( 'animate', get_template_directory_uri() . '/assets/css/animate.css', array(), $the_theme->get( 'Version' ), 'all' );
+		wp_enqueue_style( 'animate-css', get_template_directory_uri() . '/assets/css/animate.css', array(), $the_theme->get( 'Version' ), 'all' );
 
-		wp_enqueue_style( 'fontawesome-all', get_template_directory_uri() . '/assets/css/all.css', array(), $the_theme->get( 'Version' ), 'all' );
+		wp_enqueue_style( 'fontawesome-all-css', get_template_directory_uri() . '/assets/css/all.css', array(), $the_theme->get( 'Version' ), 'all' );
 
-		wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), $the_theme->get( 'Version' ), 'all' );
+		wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), $the_theme->get( 'Version' ), 'all' );
 
-		wp_enqueue_style( 'boxicons', get_template_directory_uri() . '/assets/css/boxicons.min.css', array(), $the_theme->get( 'Version' ), 'all' );
+		wp_enqueue_style( 'boxicons-css', get_template_directory_uri() . '/assets/css/boxicons.min.css', array(), $the_theme->get( 'Version' ), 'all' );
 
-		wp_enqueue_style( 'bootstrap-icons', get_template_directory_uri() . '/assets/css/bootstrap-icons.css', array(), $the_theme->get( 'Version' ), 'all' );
+		wp_enqueue_style( 'bootstrap-icons-css', get_template_directory_uri() . '/assets/css/bootstrap-icons.css', array(), $the_theme->get( 'Version' ), 'all' );
 
-		wp_enqueue_style( 'jquery-ui', get_template_directory_uri() . '/assets/css/jquery-ui.css', array(), $the_theme->get( 'Version' ), 'all' );
+		wp_enqueue_style( 'jquery-ui-css', get_template_directory_uri() . '/assets/css/jquery-ui.css', array(), $the_theme->get( 'Version' ), 'all' );
 
-		wp_enqueue_style( 'swiper-bundle', get_template_directory_uri() . '/assets/css/swiper-bundle.css', array(), $the_theme->get( 'Version' ), 'all' );
+		wp_enqueue_style( 'swiper-bundle-css', get_template_directory_uri() . '/assets/css/swiper-bundle.css', array(), $the_theme->get( 'Version' ), 'all' );
 
-		wp_enqueue_style( 'slick', get_template_directory_uri() . '/assets/css/slick.css', array(), $the_theme->get( 'Version' ), 'all' );
+		wp_enqueue_style( 'slick-css', get_template_directory_uri() . '/assets/css/slick.css', array(), $the_theme->get( 'Version' ), 'all' );
 
-		wp_enqueue_style( 'nice-select', get_template_directory_uri() . '/assets/css/nice-select.css', array(), $the_theme->get( 'Version' ), 'all' );
+		wp_enqueue_style( 'nice-select-css', get_template_directory_uri() . '/assets/css/nice-select.css', array(), $the_theme->get( 'Version' ), 'all' );
 
-		wp_enqueue_style( 'magnific-popup', get_template_directory_uri() . '/assets/css/magnific-popup.css', array(), $the_theme->get( 'Version' ), 'all' );
+		wp_enqueue_style( 'magnific-popup-css', get_template_directory_uri() . '/assets/css/magnific-popup.css', array(), $the_theme->get( 'Version' ), 'all' );
 		
-		wp_enqueue_style( 'jquery-fancybox', get_template_directory_uri() . '/assets/css/jquery.fancybox.min.css', array(), $the_theme->get( 'Version' ), 'all' );
+		wp_enqueue_style( 'jquery-fancybox-css', get_template_directory_uri() . '/assets/css/jquery.fancybox.min.css', array(), $the_theme->get( 'Version' ), 'all' );
 
-		wp_enqueue_style( 'odometer', get_template_directory_uri() . '/assets/css/odometer.css', array(), $the_theme->get( 'Version' ), 'all' );
+		wp_enqueue_style( 'odometer-css', get_template_directory_uri() . '/assets/css/odometer.css', array(), $the_theme->get( 'Version' ), 'all' );
 
-		wp_enqueue_style( 'egns-blog-page', get_template_directory_uri() . '/assets/css/blog-and-page.css', array(), $the_theme->get( 'Version' ), 'all' );
+		wp_enqueue_style( 'egns-blog-page-css', get_template_directory_uri() . '/assets/css/blog-and-page.css', array(), $the_theme->get( 'Version' ), 'all' );
 		
-		wp_enqueue_style('egns-woocommerce', get_template_directory_uri() . '/assets/css/woocommerce-custom.css', array(), $the_theme->get('Version'), 'all');
+		wp_enqueue_style('egns-woocommerce-css', get_template_directory_uri() . '/assets/css/woocommerce-custom.css', array(), $the_theme->get('Version'), 'all');
 
-		wp_enqueue_style( 'egns-typography', get_template_directory_uri() . '/assets/css/typography.css', array(), $the_theme->get( 'Version' ), 'all' );
+		wp_enqueue_style( 'egns-typography-css', get_template_directory_uri() . '/assets/css/typography.css', array(), $the_theme->get( 'Version' ), 'all' );
 
-		wp_enqueue_style( 'egns-default', get_template_directory_uri() . '/assets/css/default.css', array(), $the_theme->get( 'Version' ), 'all' );
+		wp_enqueue_style( 'egns-default-css', get_template_directory_uri() . '/assets/css/default.css', array(), $the_theme->get( 'Version' ), 'all' );
 
-		wp_enqueue_style( 'egns-style', get_template_directory_uri() . '/assets/css/style.css', array(), $the_theme->get( 'Version' ), 'all' );
+		wp_enqueue_style( 'egns-style-css', get_template_directory_uri() . '/assets/css/style.css', array(), $the_theme->get( 'Version' ), 'all' );
 
-        wp_enqueue_style( 'egns-theme', get_stylesheet_uri());
+        wp_enqueue_style( 'egns-theme-css', get_stylesheet_uri());
 		
 		// Scripts
 
-		wp_enqueue_script( 'jquery-ui', get_template_directory_uri() . '/assets/js/jquery-ui.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
+		wp_enqueue_script( 'jquery-ui-js', get_template_directory_uri() . '/assets/js/jquery-ui.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
 
-		wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
+		wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
 				
-		wp_enqueue_script( 'swiper', get_template_directory_uri() . '/assets/js/swiper-bundle.min.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
+		wp_enqueue_script( 'swiper-js', get_template_directory_uri() . '/assets/js/swiper-bundle.min.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
 		
-		wp_enqueue_script( 'slick', get_template_directory_uri() . '/assets/js/slick.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
+		wp_enqueue_script( 'slick-js', get_template_directory_uri() . '/assets/js/slick.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
 		
-		wp_enqueue_script( 'nice-select', get_template_directory_uri() . '/assets/js/jquery.nice-select.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
+		wp_enqueue_script( 'nice-select-js', get_template_directory_uri() . '/assets/js/jquery.nice-select.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
 		
-		wp_enqueue_script( 'jquery-fancybox', get_template_directory_uri() . '/assets/js/jquery.fancybox.min.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
+		wp_enqueue_script( 'jquery-fancybox-js', get_template_directory_uri() . '/assets/js/jquery.fancybox.min.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
 		
-		wp_enqueue_script( 'odometer', get_template_directory_uri() . '/assets/js/odometer.min.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
+		wp_enqueue_script( 'odometer-js', get_template_directory_uri() . '/assets/js/odometer.min.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
 
-		wp_enqueue_script( 'viewport', get_template_directory_uri() . '/assets/js/viewport.jquery.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
+		wp_enqueue_script( 'viewport-js', get_template_directory_uri() . '/assets/js/viewport.jquery.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
 		
-		wp_enqueue_script( 'isotope', get_template_directory_uri() . '/assets/js/isotope.pkgd.min.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
+		wp_enqueue_script( 'isotope-js', get_template_directory_uri() . '/assets/js/isotope.pkgd.min.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
 		
-		wp_enqueue_script( 'smooth-scroll', get_template_directory_uri() . '/assets/js/SmoothScroll.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
+		wp_enqueue_script( 'smooth-scroll-js', get_template_directory_uri() . '/assets/js/SmoothScroll.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
 		
-		wp_enqueue_script( 'magnific-popup', get_template_directory_uri() . '/assets/js/jquery.magnific-popup.min.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
+		wp_enqueue_script( 'magnific-popup-js', get_template_directory_uri() . '/assets/js/jquery.magnific-popup.min.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
 		
-		wp_enqueue_script( 'imagesloaded', get_template_directory_uri() . '/assets/js/imagesloaded.pkgd.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
+		wp_enqueue_script( 'imagesloaded-js', get_template_directory_uri() . '/assets/js/imagesloaded.pkgd.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
 		
-		wp_enqueue_script( 'masonry', get_template_directory_uri() . '/assets/js/masonry.pkgd.min.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
+		wp_enqueue_script( 'masonry-js', get_template_directory_uri() . '/assets/js/masonry.pkgd.min.js', array( 'jquery' ), $the_theme->get( 'Version' ), true );
 		
-		wp_enqueue_script( 'egns-main', get_template_directory_uri() . '/assets/js/main.js', array(), $the_theme->get( 'Version' ), true );
+		wp_enqueue_script( 'egns-main-js', get_template_directory_uri() . '/assets/js/main.js', array(), $the_theme->get( 'Version' ), true );
 
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
